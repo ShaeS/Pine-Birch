e41a78b5ab689331664c1522d3fa9560778e9e66
ShaeS <shaescotten@gmail.com>
2016-01-16 15:17:22 -0800

    making admin popup

--- js/admin.js
+++ js/admin.js
@@ -5,7 +5,9 @@ $(".editbutton").click(function() {
   var popupitem = $("<div class='popupitem'></div>");
   $(popupscreen).append(popupitem);
   
-  var popupform = $("<form><label for='pname'>name:</label><input id='pname' type='text'><label for='pcategory'>name:</label><select><option value='all'>all</option><option value='instruments'>instruments</option><option value='vehicles'>vehicles</option><option value='animals'>animals</option></select></form>");
+  var popupform = $("<form><label for='pname'>name:</label><input id='pname' type='text'><label for='pcategory'>category:</label><select><option value='all'>all</option><option value='instruments'>instruments</option><option value='vehicles'>vehicles</option><option value='animals'>animals</option></select><br/></form>");
   $(popupitem).append(popupform);
+  
+  $(document.body).append(popupscreen);
   
 });
