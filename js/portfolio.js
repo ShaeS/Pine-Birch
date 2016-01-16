e9a82f3e58f9cf129cb528c768d524522aba2abd
ShaeS <shaescotten@gmail.com>
2016-01-16 10:19:44 -0800

    responsive popup work

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,6 @@
 $("#imagewrapper ul li").click(function() {
   var popupscreen = $("<div class='popupscreen'></div>");
-  $(document.body).append(popupscreen);
+ 
   
   var popupitem = $("<div class='popupitem'></div>");
   $(popupscreen).append(popupitem);
@@ -13,6 +13,24 @@ $("#imagewrapper ul li").click(function() {
   $(popupitem).append(popuptextdiv);
   
   var h2temp = $(this).children("div").children("p").text();
-  var popuptitle = $("<h2 class='popuptitle'></h2>").text(h2temp);
+  var popuptitle = $("<h2 class='popuptitle'>Product title</h2><hr class='popuphr'><p class='popupprice'>$35.00</p>");
   $(popuptextdiv).append(popuptitle);
+  
+  var popupdescription = $("<p class='popupdescription'>This product is made entirely out of pine and birch, and is crafted to be durable through any situation. it is perfect for children, or collectors of finely crafted wooden masterpieces. You'll love it!</p>");
+  $(popuptextdiv).append(popupdescription);
+  
+  var popupbutton = $("<a href='#' class='popupbutton'>add to cart</a>");
+  $(popuptextdiv).append(popupbutton);
+  
+  var closebutton = $("<span class='closebutton'>x</span>");
+  $(popupitem).append(closebutton);
+  
+   $(document.body).append(popupscreen);
+  
+  $(closebutton).click(function() {
+    $(popupscreen).remove(); 
+  });
+  $(popupscreen).click(function() {
+    $(popupscreen).remove(); 
+  });
 });
