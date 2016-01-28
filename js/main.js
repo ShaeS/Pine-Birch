eb599f35519932f71bf13f2d00aa8427a834a8b0
ShaeS <shaescotten@gmail.com>
2016-01-28 15:47:48 -0800

    added animation

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".animsition").animsition({
+    inClass               :   'fade-in',
+    outClass              :   'fade-out',
+    inDuration            :    400,
+    outDuration           :    200,
+    linkElement           :   '.animsition-link', 
+    touchSupport          :    true, 
+    loading               :    true,
+    loadingParentElement  :   'body', //animsition wrapper element
+    loadingClass          :   'animsition-loading',
+    unSupportCss          : [ 'animation-duration',
+                              '-webkit-animation-duration',
+                              '-o-animation-duration'
+                            ]
+  });
+});
